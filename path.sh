fa424967df4bf1daa0d6121cfb941f892601c154
W. Zielinski <ziewit@gmail.com>
2016-05-30 00:04:35 +0200

    First project push

---
+++ path.sh
@@ -0,0 +1,14 @@
+# Defining Kaldi root directory
+export KALDI_ROOT=`pwd`/../../..
+
+# Setting paths to useful tools
+export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$PWD:$PATH
+
+# Defining audio data directory (modify it for your installation directory!)
+export DATA_ROOT="/home/witek/Kaldi/kaldi-trunk/egs/digits/s5/digits_audio_PL"
+
+# Variable that stores path to MITLM library
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/tools/mitlm-svn/lib
+
+# Variable needed for proper data sorting
+export LC_ALL=C
